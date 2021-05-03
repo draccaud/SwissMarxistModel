5fca8f4c2127e339c9544b88deecd664f88d0f4a
Joakim <martinsjoakim@outlook.com>
2021-05-03 21:57:05 +0200

    Refonte totale du code Les graphes sont désormais affiché dans QtPy

--- main.py
+++ main.py
@@ -1,32 +1,46 @@
+import random
 import sys
+
 import matplotlib
-matplotlib.use('Qt5Agg')
-
+import matplotlib.figure
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-from PyQt5 import QtCore, QtGui, QtWidgets
-
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT as NavigationToolbar
-from matplotlib.figure import Figure
-from matplotlib import rc
-
-class MplCanvas(FigureCanvasQTAgg):
-
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-        super(MplCanvas, self).__init__(fig)
+import pandas as pd
+import squarify
+from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QApplication
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 
-class MainWindow(QtWidgets.QMainWindow):
+class PrettyWidget(QWidget):
+    def __init__(self):
+        super(PrettyWidget, self).__init__()
+        self.initUI()
 
-    def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
+    def initUI(self):
+        self.setGeometry(100, 100, 800, 600)
 
-        # y-axis in bold
-        rc('font', weight='bold')
+        grid = QGridLayout()
+        self.setLayout(grid)
+
+        btn1 = QPushButton('Plot 1 ', self)
+        btn1.resize(btn1.sizeHint())
+        btn1.clicked.connect(self.plot1)
+        grid.addWidget(btn1, 5, 0)
+
+        btn2 = QPushButton('Plot 2 ', self)
+        btn2.resize(btn2.sizeHint())
+        # btn2.clicked.connect(self.plot2)
+        grid.addWidget(btn2, 5, 1)
+
+        self.figure = matplotlib.figure.Figure()
+        self.canvas = FigureCanvas(self.figure)
+        grid.addWidget(self.canvas, 3, 0, 1, 2)
+
+        self.show()
+
+    def plot1(self):
+        self.figure.clf()
+
+        ax1 = self.figure.add_subplot(211)
 
         # Values of each group
         bars1 = [12, 28, 1, 8, 22]
@@ -39,39 +53,47 @@ class MainWindow(QtWidgets.QMainWindow):
         # The position of the bars on the x-axis
         r = [0, 1, 2, 3, 4]
 
-        # Names of group and bar width
-        names = ['A', 'B', 'C', 'D', 'E']
-        barWidth = 1
+        bar_width = 1
 
         # Create brown bars
-        plt.bar(r, bars1, color='#7f6d5f', edgecolor='white', width=barWidth)
+        ax1.axes.bar(r, bars1, color='#7f6d5f', edgecolor='white', width=bar_width)
         # Create green bars (middle), on top of the first ones
-        plt.bar(r, bars2, bottom=bars1, color='#557f2d', edgecolor='white', width=barWidth)
+        ax1.axes.bar(r, bars2, bottom=bars1, color='#557f2d', edgecolor='white', width=bar_width)
         # Create green bars (top)
-        plt.bar(r, bars3, bottom=bars, color='#2d7f5e', edgecolor='white', width=barWidth)
+        ax1.axes.bar(r, bars3, bottom=bars, color='#2d7f5e', edgecolor='white', width=bar_width)
 
-        # Custom X axis
-        plt.xticks(r, names, fontweight='bold')
-        plt.xlabel("group")
+        ax1.plot()
 
-        # Show graphic
-        plt.show()
+        ax2 = self.figure.add_subplot(234)
+        df = pd.DataFrame({'nb_people': [random.randint(1, 10) for _ in range(4)],
+                           'group': ["group A", "group B", "group C", "group D"]})
 
-        # Create toolbar, passing canvas as first parament, parent (self, the MainWindow) as second.
-        toolbar = NavigationToolbar(plt, self)
+        squarify.plot(sizes=df['nb_people'], label=df['group'], alpha=.8, ax=ax2)
+        ax2.axis('off')
 
-        layout = QtWidgets.QVBoxLayout()
-        layout.addWidget(toolbar)
+        ax3 = self.figure.add_subplot(235)
+        df = pd.DataFrame({'nb_people': [random.randint(1, 10) for _ in range(4)],
+                           'group': ["group A", "group B", "group C", "group D"]})
+
+        squarify.plot(sizes=df['nb_people'], label=df['group'], alpha=.8, ax=ax3)
+        ax3.axis('off')
+
+        ax4 = self.figure.add_subplot(236)
+        df = pd.DataFrame({'nb_people': [random.randint(1, 10) for _ in range(4)],
+                           'group': ["group A", "group B", "group C", "group D"]})
+
+        squarify.plot(sizes=df['nb_people'], label=df['group'], alpha=.8, ax=ax4)
+        ax4.axis('off')
+
+        self.canvas.draw()
+
+        self.canvas.draw_idle()
+
+    def plot2(self):
+        return
 
 
-        # Create a placeholder widget to hold our toolbar and canvas.
-        widget = QtWidgets.QWidget()
-        widget.setLayout(layout)
-        self.setCentralWidget(widget)
-
-        self.show()
-
-
-app = QtWidgets.QApplication(sys.argv)
-w = MainWindow()
-app.exec_()
+app = QApplication(sys.argv)
+app.aboutToQuit.connect(app.deleteLater)
+GUI = PrettyWidget()
+sys.exit(app.exec_())

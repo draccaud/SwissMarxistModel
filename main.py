84c72772ed43adff52e932aff85841b0cac235e8
David Raccaud <david@raccaud.ch>
2021-05-08 12:22:43 +0200

    Mise à jour de David le 8 mai

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ import squarify
 from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QApplication
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
+#Définition des variables
+#Pour le moment statique, a faire en sorte qu'elles ne le soit pas par
+Compo_org_1 = 2.22
+Compo_org_2 = 2.22
+Taux_expl_1 = 0.35
+Taux_expl_2 = 0.35
+Total_t = 1496
+
 
 def plotGraphSectors(figure, data):
     """
@@ -40,6 +48,52 @@ def plotGraphSectors(figure, data):
     #Définit la grille
     sectorsGraph.grid(True, 'major', 'y', ls='--', lw=.5, c='k', alpha=.3)
 
+
+def CalcYear():
+    C_1_t = (Total_t / 2.72) * 1.72
+    V_1_t = C_1_t / Compo_org_1
+    S_1_t = V_1_t * Taux_expl_1
+    Tot_1_t = C_1_t + V_1_t + S_1_t
+
+    C_2_t = Total_t / 2.72
+    V_2_t = C_2_t / Compo_org_2
+    S_2_t = V_2_t * Taux_expl_2
+    Tot_2_t = C_2_t + V_2_t + S_2_t
+
+    C_3_t = C_1_t + C_2_t
+    V_3_t = V_1_t + V_2_t
+    S_3_t = S_1_t + S_2_t
+    Tot_3_t = C_3_t + V_3_t + S_3_t
+
+    Acc_t = Tot_1_t - (C_1_t + C_2_t)
+    """""
+    #On convertit tout en int
+    C_1_t = int(C_1_t)
+    V_1_t = int(V_1_t)
+    S_1_t = int(S_1_t)
+    C_2_t = int(C_2_t)
+    V_2_t = int(V_2_t)
+    S_2_t = int(S_2_t)
+    C_3_t = int(C_3_t)
+    V_3_t = int(V_3_t)
+    S_3_t = int(S_3_t)
+    """""
+
+    TabResultat = [[C_1_t, V_1_t, S_1_t], [C_2_t, V_2_t, S_2_t], [C_3_t, V_3_t, S_3_t]]
+    return TabResultat
+
+def gettotal(TabResultats):
+    Total = 0
+    for i in range(len(TabResultats)):
+        Total =  Total + TabResultats[i]
+    return Total
+
+
+def dynemisesizeaquar(value, total):
+    AffValue = value / total
+    AffPro = (1 - AffValue) / 3
+    AffichageDynamique = [AffPro, AffValue, AffPro, AffPro]
+    return AffichageDynamique
 
 class PrettyWidget(QWidget):
     def __init__(self):
@@ -74,27 +128,36 @@ class PrettyWidget(QWidget):
     def plot2(self):
         self.figure.clf()
 
-        data = np.array([[588, 265, 93], [342, 154, 54], [930, 419, 147]])
+        #On fait les calculs pour l'année une
+        DataYearOne = CalcYear()
+
+        data = np.array(DataYearOne)
 
         #David utilise cette fonction pour dessiner le graph
         plotGraphSectors(self.figure, data)
 
         ####################################################
 
+        Tot_1_t = gettotal(DataYearOne[0])
+        Tot_2_t = gettotal(DataYearOne[1])
+        Tot_3_t = gettotal(DataYearOne[2])
+
+
         ax2 = self.figure.add_subplot(234)
 
-        squarify.plot(sizes=[1, 2], label=["Total", ""], color=["#3498db", "white"], alpha=.8, ax=ax2)
+        squarify.plot(sizes=dynemisesizeaquar(Tot_1_t, Tot_3_t), label=["", "Total", "", ""], color=["white", "#3498db", "white", "white"], alpha=.8, ax=ax2)
         ax2.axis('off')
 
         ax3 = self.figure.add_subplot(235)
-        squarify.plot(sizes=[2, 1], label=['Total', ""], color=['#e74c3c', "white"], alpha=.8, ax=ax3)
+        squarify.plot(sizes=dynemisesizeaquar(Tot_2_t, Tot_3_t), label=["", "Total", "", ""], color=["white", "#e74c3c", "white", "white"], alpha=.8, ax=ax3)
         ax3.axis('off')
 
         ax4 = self.figure.add_subplot(236)
-        squarify.plot(sizes=[1], label=['Total'], color=['#7f8c8d'], alpha=.8, ax=ax4)
+        squarify.plot(sizes=[Tot_3_t], label=['Total'], color=['#7f8c8d'], alpha=.8, ax=ax4)
         ax4.axis('off')
 
         self.canvas.draw()
+
 
 
 app = QApplication(sys.argv)
